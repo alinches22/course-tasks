704f088d34c3c717711f359fd7138562073e181f
alinches22 <aaliinkaa488@gmail.com>
2024-10-22 10:57:55 +0000

    Updated count_files.sh to handle permission errors

--- count_files.sh
+++ count_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-file_count=$(find /etc -type f -not -path "/etc/polkit-1/*" -not -path "/etc/credstore/*" -not -path "/etc/ssl/private/*" | wc -l)
+# Підрахунок кількості файлів, ігноруючи проблемні директорії
+file_count=$(find /etc -type f -not -path "/etc/polkit-1/*" -not -path "/etc/credstore/*" -not -path "/etc/ssl/private/*" 2>/dev/null | wc -l)
 
 echo "Кількість звичайних файлів у /etc: $file_count"
+
 
