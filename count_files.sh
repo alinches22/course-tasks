797d16da6f9e3f112ec36a79c0b3f52f878b2083
alinches22 <aaliinkaa488@gmail.com>
2024-10-22 11:26:26 +0000

    Add count_files.sh and GitHub Actions workflow

--- count_files.sh
+++ count_files.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-file_count=$(find /etc -type f -not -path "/etc/polkit-1/*" -not -path "/etc/credstore/*" -not -path "/etc/ssl/private/*" | wc -l)
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
 
-echo "Кількість звичайних файлів у /etc: $file_count"
+usage() {
+    echo "Usage: $0 --check_dir=<directory>"
+    exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+fi
+
+if [[ $1 =~ ^--check_dir=(.*) ]]; then
+    DIRECTORY="${BASH_REMATCH[1]}"
+else
+    usage
+fi
+
+if [ ! -d "$DIRECTORY" ]; then
+    echo "The directory $DIRECTORY does not exist."
+    exit 2
+fi
+
+FILE_COUNT=$(find "$DIRECTORY" -type f | wc -l)
+echo "Number of files in $DIRECTORY: $FILE_COUNT"
+
 
 
 
